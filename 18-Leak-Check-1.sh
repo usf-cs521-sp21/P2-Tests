bab51f241f3d62091eba8537af17d01a9ba00120
Matthew Malensek <matt@malensek.net>
2021-04-05 13:57:53 -0700

    Add leak check

---
+++ 18-Leak-Check-1.sh
@@ -0,0 +1,62 @@
+source "${TEST_DIR}/lib/funcs.bash"
+
+fd_check='
+{
+    if (/==[0-9]*==\s*Open file descriptor/) {
+        fd=$0;
+        getline;
+        if (! /inherited/) {
+            print fd
+        }
+    }
+}' 
+
+script=$(cat <<EOM
+ls /
+!1
+
+
+jobs
+cd
+ls / # Doing some more lsing
+!!
+asdfghjklqprewopiqwualasdf # Bad Command!
+# Comment only
+pwd
+echo hi
+sort -r -n < /etc/passwd > /tmp/$RANDOM
+rm /tmp/$RANDOM
+history
+!p
+
+
+echo bye
+EOM
+)
+
+test_start "Memory Leak Check"
+
+if ! ( which valgrind &> /dev/null ); then
+    # "valgrind is not installed. Please install (as root) with:"
+    # "pacman -Sy valgrind"
+    test_end 1
+fi
+
+leak_output=$(timeout ${run_timeout} valgrind \
+    --trace-children=no \
+    --child-silent-after-fork=yes \
+    --leak-check=full \
+    --track-fds=yes \
+    --track-origins=yes \
+    ./$SHELL_NAME < <(echo "${script}") 2>&1)
+
+# Check for open FDs
+awk "${fd_check}" <<< "${leak_output}" \
+    | grep -i '==[0-9]*==.*file descriptor' && test_end 1
+
+# Make sure there were no leaks possible
+grep -i '==[0-9]*==.*no leaks are possible' \
+    <<< "${leak_output}" || test_end 1
+
+# If none of the conditions were triggered above, the test passes.
+test_end 0
